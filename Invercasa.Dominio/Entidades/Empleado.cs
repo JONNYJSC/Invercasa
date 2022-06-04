a5a8f52cd657394bb994f987e61ee42ac63d5b30
JONNYJSC <jjonath21@gmail.com>
2022-06-04 11:21:01 -0600

    Agregando Entidades

---
+++ Invercasa.Dominio/Entidades/Empleado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invercasa.Dominio.Entidades
+{
+    public class Empleado
+    {
+        public Empleado(string nombre, string tipoIdentificacion, string numeroIdentificacion)
+        {
+            Nombre = nombre;
+            TipoIdentificacion = tipoIdentificacion;
+            NumeroIdentificacion = numeroIdentificacion;
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string TipoIdentificacion { get; set; }
+        public string NumeroIdentificacion { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public decimal SalarioBaseMensual { get; set; }
+    }
+}
---
+++ Invercasa.Dominio/Entidades/Vacaciones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invercasa.Dominio.Entidades
+{
+    public class Vacaciones
+    {
+        public int Id { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public int IdEmpleado { get; set; }
+    }
+}
