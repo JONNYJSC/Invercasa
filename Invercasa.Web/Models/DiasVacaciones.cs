29786ace40ef3d4b653e9fdb8a061984211d3bca
JONNYJSC <jjonath21@gmail.com>
2022-06-06 02:24:04 -0600

    Agregando vistas  y controlador

---
+++ Invercasa.Web/Models/DiasVacaciones.cs
@@ -0,0 +1,8 @@
+﻿namespace Invercasa.Web.Models
+{
+    public class DiasVacaciones
+    {
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+    }
+}
