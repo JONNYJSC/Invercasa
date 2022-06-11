c4430eba76cf53f5962e4a3f84fa4d9ae1a3c246
JONNYJSC <jjonath21@gmail.com>
2022-06-11 13:22:19 -0600

    Arreglando ruta para ErrorPage en program.cs

--- Invercasa.Web/Program.cs
+++ Invercasa.Web/Program.cs
@@ -25,12 +25,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Empleado/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+} else
+{
+    app.UseStatusCodePagesWithReExecute("/Error/{0}");
 }
 
-app.UseStatusCodePagesWithRedirects("Empleado/Error={0}");
+//app.UseStatusCodePagesWithRedirects("Empleado/Error={0}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
